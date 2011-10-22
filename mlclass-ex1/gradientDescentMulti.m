617423e30524cb9bcec5c362b8237277e9cb2c5b
Nicolas Rolland <nicolas.rolland@gmail.com>
2011-10-22 22:36:02 +0200

    gradientdescentMulti

--- mlclass-ex1/gradientDescentMulti.m
+++ mlclass-ex1/gradientDescentMulti.m
@@ -18,13 +18,8 @@ for iter = 1:num_iters
     %
 
 
-
-
-
-
-
-
-
+    %size((X*theta - y)'*X)
+    theta = theta - alpha * (1/m * (X*theta - y)'*X)';
 
 
     % ============================================================
